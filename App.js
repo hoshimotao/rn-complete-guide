c94fe1cde9ef23f010a1202ef9e00ad2a62dba20
Hoshimotao <jthomas302@gmail.com>
2019-12-18 00:23:01 -0500

    add button and text input

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-     
-         <View> Hello There, User. </View>
-        <Button> Submit </Button>
-      </View>
+    <View style={{ padding:30 }}>
+     <View> 
+       <TextInput placeholder="Course Goal" style={{borderColor: 'gray', borderBottomWidth: 1, margin:10 }}/>
+       <Button  title="ADD"/>
+         </View>        
+      <View>  
+        </View>
+        </View>
 
      
 
