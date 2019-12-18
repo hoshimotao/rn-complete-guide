01b078f2c853f090384ba4c8946f9843b3010535
Hoshimotao <jthomas302@gmail.com>
2019-12-17 22:45:32 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     
+         
+        <Button> Submit </Button>
+      </View>
+
+     
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
