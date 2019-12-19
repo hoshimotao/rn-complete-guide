095fb1f5e956da45731c93825e044e9e6ebddcd9
Hoshimotao <jthomas302@gmail.com>
2019-12-18 23:56:51 -0500

    lean out styles with StyleSheet create

--- App.js
+++ App.js
@@ -3,23 +3,34 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding:30 }}>
-     <View style={{flexDirection: 'row', justifyContent: 'center'}}> 
-       <TextInput placeholder="Course Goal" style={{borderColor: 'gray', borderBottomWidth: 1, margin:10 }}/>
-       <Button  title="ADD"/>
-         </View>  
-             
-           
-      <View>  
-        </View>
-        </View>
 
-     
+
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}> 
+       <TextInput placeholder="Course Goal" style={styles.input}/>
+       <Button  title="ADD"/>
+      </View>  
+    </View>      
 
     
   );
 }
 
 const styles = StyleSheet.create({
- 
+  screen: {
+    padding: 30
+  },
+  inputContainer: {
+    marginTop: 20, 
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding:8,
+    fontSize: 16
+  }
 });
