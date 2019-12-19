eb0b4afe4d0e1873f432a876abc121287efe23d7
Hoshimotao <jthomas302@gmail.com>
2019-12-18 22:58:34 -0500

    center object

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{ padding:30 }}>
-     <View style={{ flexDirection: "row"}}> 
+     <View style={{flexDirection: 'row', justifyContent: 'center'}}> 
        <TextInput placeholder="Course Goal" style={{borderColor: 'gray', borderBottomWidth: 1, margin:10 }}/>
        <Button  title="ADD"/>
-         </View>        
+         </View>  
+             
+           
       <View>  
         </View>
         </View>
