9811b57cb6797a30daec793fcf5ba0ef3a0af862
Hoshimotao <jthomas302@gmail.com>
2020-01-21 00:49:51 -0500

    add scroll view

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) =>{
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler =() => {
+    setCourseGoals(currentGoals => [...courseGoals, enteredGoal])
+    console.log(enteredGoal)
+
+  };
+
+
   return (
 
 
     <View style={styles.screen}>
       <View style={styles.inputContainer}> 
-       <TextInput placeholder="Course Goal" style={styles.input}/>
-       <Button  title="ADD"/>
+       <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} 
+       value={enteredGoal}/>
+
+       {/* the add button */}
+       <Button  title="ADD" onPress={addGoalHandler}/>
       </View>  
+
+      <ScrollView>
+        {courseGoals.map((goal) => <View key={goal}><Text style={styles.listItem}> {goal} </Text></View>)}
+      </ScrollView>
+
     </View>      
 
     
@@ -31,6 +53,15 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     padding:8,
-    fontSize: 16
+    fontSize: 16,
+    borderRadius: 4
+  },
+  listItem: {
+    padding: 10,
+    marginTop: 10,
+    backgroundColor: 'lightgray',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 4
   }
 });
---
+++ README.md
@@ -0,0 +1,9 @@
+- It's a good idea to wrap <Text> elements in a <View> element for more styling options.
+
+- ScrollView is another important component in ReactNative and must be explicitly imported at the top ---> 
+
+import { StyleSheet, Text, View, TextInput, Button, ScrollView })
+
+- by adding horizontal as an attribute, you can scroll sideways if necessary.
+
+"<ScrollView horizontal>"
